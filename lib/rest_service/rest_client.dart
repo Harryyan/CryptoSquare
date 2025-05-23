37597cdd97c3d32e37b4c8e85e5699c8dd486d9b
Harryyan <harryyan1238@gmail.com>
2025-05-24 07:34:15 +1200

    update proxy ip

--- lib/rest_service/rest_client.dart
+++ lib/rest_service/rest_client.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:cryptosquare/util/storage.dart';
@@ -152,7 +153,7 @@ abstract class RestClient {
     //         // Config the client.
     //         client.findProxy = (uri) {
     //           // Forward all request to proxy "localhost:8888".
-    //           return 'PROXY 127.0.0.1:9090';
+    //           return 'PROXY 192.168.1.123:9090';
     //         };
     //         // You can also create a new HttpClient for Dio instead of returning,
     //         // but a client must being returned here.
