ad997b9d7cf1520d6af9d9de2c0405de26a05bb9
Harryyan <harryyan1238@gmail.com>
2025-06-02 08:38:54 +1200

    add new ui of sharing

--- lib/widget/social_share_widget.dart
+++ lib/widget/social_share_widget.dart
@@ -32,180 +32,292 @@ class SocialShareWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> itemWidgets = [
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () async {
-          await _shareToFacebook(context);
-          Navigator.of(context).pop();
-        },
-        icon: Image.asset("assets/images/facebook-share.png"),
-      ),
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () async {
-          await _shareToTwitter(context);
-          Navigator.of(context).pop();
-        },
-        icon: Image.asset("assets/images/twitter-share.png"),
-      ),
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () async {
-          String shareDesc = desc ?? "";
-          if (shareDesc.length > 100) {
-            shareDesc = shareDesc.substring(0, 100);
-          }
-          if (imgUrl != null) {
-            Uint8List bytes =
-                (await NetworkAssetBundle(
-                  Uri.parse(imgUrl!),
-                ).load(imgUrl!)).buffer.asUint8List();
-            IMG.Image? img = IMG.decodeImage(bytes);
-            Directory documentDirectory =
-                await getApplicationDocumentsDirectory();
-
-            IMG.Image resized = IMG.copyResizeCropSquare(
-              img!,
-              size: 100,
-              interpolation: IMG.Interpolation.cubic,
-            );
-            Uint8List resizedImg = Uint8List.fromList(IMG.encodePng(resized));
-
-            String postfix = "imagetest.png";
-
-            if (imgUrl!.endsWith("png")) {
-              postfix = "imagetest.png";
-            } else if (imgUrl!.endsWith("jpg")) {
-              postfix = "imagetest.jpg";
-            } else if (imgUrl!.endsWith("jpeg")) {
-              postfix = "imagetest.jpeg";
-            } else if (imgUrl!.endsWith("gif")) {
-              postfix = "imagetest.gif";
-            }
-
-            File file = File(join(documentDirectory.path, postfix));
-            file.writeAsBytesSync(resizedImg);
-
-            ShareParamsBean bean = ShareParamsBean(
-              contentType: LaShareContentTypes.webpage,
-              platform: SharePlatforms.wechatSession,
-              webUrl: url,
-              title: title,
-              imageFilePath: file.path,
-              text: shareDesc,
-            );
-            // ignore: use_build_context_synchronously
-            _share(bean, context);
-          } else {
-            ShareParamsBean bean = ShareParamsBean(
-              contentType: LaShareContentTypes.webpage,
-              platform: SharePlatforms.wechatSession,
-              webUrl: url,
-              imageFilePath: "assets/images/avatar.png",
-              title: title,
-              text: shareDesc,
-            );
-            _share(bean, context);
-          }
-
-          Navigator.of(context).pop();
-        },
-        icon: Image.asset("assets/images/wechat-share.png"),
-      ),
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () async {
-          if (imgUrl != null) {
-            Uint8List bytes =
-                (await NetworkAssetBundle(
-                  Uri.parse(imgUrl!),
-                ).load(imgUrl!)).buffer.asUint8List();
-            IMG.Image? img = IMG.decodeImage(bytes);
-            IMG.Image resized = IMG.copyResizeCropSquare(
-              img!,
-              size: 100,
-              interpolation: IMG.Interpolation.cubic,
-            );
-            Uint8List resizedImg = Uint8List.fromList(IMG.encodePng(resized));
-            Directory documentDirectory =
-                await getApplicationDocumentsDirectory();
-            File file = File(
-              join(
-                documentDirectory.path,
-                imgUrl!.endsWith("png") ? 'imagetest.png' : 'imagetest.jpeg',
-              ),
-            );
-            file.writeAsBytesSync(resizedImg);
-
-            ShareParamsBean bean = ShareParamsBean(
-              contentType: LaShareContentTypes.webpage,
-              platform: SharePlatforms.wechatTimeline,
-              webUrl: url,
-              title: title,
-              imageFilePath: file.path,
-              text: desc,
-            );
-            _share(bean, context);
-          } else {
-            ShareParamsBean bean = ShareParamsBean(
-              contentType: LaShareContentTypes.webpage,
-              platform: SharePlatforms.wechatTimeline,
-              webUrl: url,
-              imageFilePath: "assets/images/avatar.png",
-              title: title,
-              text: desc,
-            );
-            _share(bean, context);
-          }
-          Navigator.of(context).pop();
-        },
-        icon: Image.asset("assets/images/session-share.png"),
-      ),
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () {
-          Clipboard.setData(ClipboardData(text: url ?? ""));
-          GFToast.showToast(
-            'Share link copy success.'.tr,
-            context,
-            toastPosition: GFToastPosition.CENTER,
-          );
-          Navigator.of(context).pop();
-        },
-        icon: SizedBox(
-          child: Icon(Icons.copy_sharp, size: 40, color: Colors.black),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
       ),
-      GFIconButton(
-        color: Colors.transparent,
-        onPressed: () async {
-          String body = (title ?? desc ?? "") + ("\n${url ?? ""}");
-          Share.share(body);
-          Navigator.pop(context);
-        },
-        icon: SizedBox(
-          child: Icon(Icons.more_horiz, size: 40, color: Colors.black),
-        ),
-      ),
-    ];
-
-    return SizedBox(
-      height: 144,
       child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: GFItemsCarousel(
-              itemHeight: 82,
-              rowCount: 6,
-              children: itemWidgets,
+          // Top handle indicator
+          Container(
+            width: 40,
+            height: 4,
+            margin: EdgeInsets.only(top: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(2),
             ),
           ),
-          const SizedBox(height: 54),
+          
+          // Share platforms grid
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 32),
+            child: Column(
+              children: [
+                // First row: WeChat, WeChat Moments, X
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildShareItem(
+                      context,
+                      "assets/images/wechat-share.png",
+                      "微信",
+                      () async => await _shareToWeChat(context),
+                    ),
+                    _buildShareItem(
+                      context,
+                      "assets/images/session-share.png", 
+                      "朋友圈",
+                      () async => await _shareToWeChatMoments(context),
+                    ),
+                    _buildShareItem(
+                      context,
+                      "assets/images/twitter-share.png",
+                      "X",
+                      () async => await _shareToTwitter(context),
+                    ),
+                  ],
+                ),
+                
+                SizedBox(height: 40),
+                
+                // Second row: Facebook, Copy Link, More
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildShareItem(
+                      context,
+                      "assets/images/facebook-share.png",
+                      "facebook",
+                      () async => await _shareToFacebook(context),
+                    ),
+                    _buildShareItem(
+                      context,
+                      "assets/images/copy-share.png",
+                      "copy link",
+                      () => _copyLink(context),
+                    ),
+                    _buildShareItem(
+                      context,
+                      null, // Use icon instead of image
+                      "更多",
+                      () => _shareToMore(context),
+                      icon: Icons.more_horiz,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          
+          // Cancel button
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[100],
+                foregroundColor: Colors.black87,
+                elevation: 0,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                "取消",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+          
+          // Bottom safe area
+          SizedBox(height: MediaQuery.of(context).padding.bottom + 20),
         ],
       ),
     );
+  }
+
+  Widget _buildShareItem(
+    BuildContext context,
+    String? imagePath,
+    String label,
+    VoidCallback onTap, {
+    IconData? icon,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.grey[50],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: imagePath != null
+                  ? Image.asset(
+                      imagePath,
+                      width: 40,
+                      height: 40,
+                      fit: BoxFit.contain,
+                    )
+                  : Icon(
+                      icon ?? Icons.share,
+                      size: 32,
+                      color: Colors.grey[600],
+                    ),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey[700],
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _shareToWeChat(BuildContext context) async {
+    String shareDesc = desc ?? "";
+    if (shareDesc.length > 100) {
+      shareDesc = shareDesc.substring(0, 100);
+    }
+    if (imgUrl != null) {
+      Uint8List bytes =
+          (await NetworkAssetBundle(
+        Uri.parse(imgUrl!),
+      ).load(imgUrl!)).buffer.asUint8List();
+      IMG.Image? img = IMG.decodeImage(bytes);
+      Directory documentDirectory =
+          await getApplicationDocumentsDirectory();
+
+      IMG.Image resized = IMG.copyResizeCropSquare(
+        img!,
+        size: 100,
+        interpolation: IMG.Interpolation.cubic,
+      );
+      Uint8List resizedImg = Uint8List.fromList(IMG.encodePng(resized));
+
+      String postfix = "imagetest.png";
+
+      if (imgUrl!.endsWith("png")) {
+        postfix = "imagetest.png";
+      } else if (imgUrl!.endsWith("jpg")) {
+        postfix = "imagetest.jpg";
+      } else if (imgUrl!.endsWith("jpeg")) {
+        postfix = "imagetest.jpeg";
+      } else if (imgUrl!.endsWith("gif")) {
+        postfix = "imagetest.gif";
+      }
+
+      File file = File(join(documentDirectory.path, postfix));
+      file.writeAsBytesSync(resizedImg);
+
+      ShareParamsBean bean = ShareParamsBean(
+        contentType: LaShareContentTypes.webpage,
+        platform: SharePlatforms.wechatSession,
+        webUrl: url,
+        title: title,
+        imageFilePath: file.path,
+        text: shareDesc,
+      );
+      // ignore: use_build_context_synchronously
+      _share(bean, context);
+    } else {
+      ShareParamsBean bean = ShareParamsBean(
+        contentType: LaShareContentTypes.webpage,
+        platform: SharePlatforms.wechatSession,
+        webUrl: url,
+        imageFilePath: "assets/images/avatar.png",
+        title: title,
+        text: shareDesc,
+      );
+      _share(bean, context);
+    }
+
+    Navigator.of(context).pop();
+  }
+
+  Future<void> _shareToWeChatMoments(BuildContext context) async {
+    if (imgUrl != null) {
+      Uint8List bytes =
+          (await NetworkAssetBundle(
+        Uri.parse(imgUrl!),
+      ).load(imgUrl!)).buffer.asUint8List();
+      IMG.Image? img = IMG.decodeImage(bytes);
+      IMG.Image resized = IMG.copyResizeCropSquare(
+        img!,
+        size: 100,
+        interpolation: IMG.Interpolation.cubic,
+      );
+      Uint8List resizedImg = Uint8List.fromList(IMG.encodePng(resized));
+      Directory documentDirectory =
+          await getApplicationDocumentsDirectory();
+      File file = File(
+        join(
+          documentDirectory.path,
+          imgUrl!.endsWith("png") ? 'imagetest.png' : 'imagetest.jpeg',
+        ),
+      );
+      file.writeAsBytesSync(resizedImg);
+
+      ShareParamsBean bean = ShareParamsBean(
+        contentType: LaShareContentTypes.webpage,
+        platform: SharePlatforms.wechatTimeline,
+        webUrl: url,
+        title: title,
+        imageFilePath: file.path,
+        text: desc,
+      );
+      _share(bean, context);
+    } else {
+      ShareParamsBean bean = ShareParamsBean(
+        contentType: LaShareContentTypes.webpage,
+        platform: SharePlatforms.wechatTimeline,
+        webUrl: url,
+        imageFilePath: "assets/images/avatar.png",
+        title: title,
+        text: desc,
+      );
+      _share(bean, context);
+    }
+    Navigator.of(context).pop();
+  }
+
+  void _copyLink(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: url ?? ""));
+    GFToast.showToast(
+      'Share link copy success.'.tr,
+      context,
+      toastPosition: GFToastPosition.CENTER,
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _shareToMore(BuildContext context) async {
+    String body = (title ?? desc ?? "") + ("\n${url ?? ""}");
+    Share.share(body);
+    Navigator.pop(context);
   }
 
   Future<void> _shareToFacebook(BuildContext context) async {
@@ -233,6 +345,7 @@ class SocialShareWidget extends StatelessWidget {
       print('Facebook share error: $e');
       await _shareToBrowser('facebook', context);
     }
+    Navigator.of(context).pop();
   }
 
   Future<void> _shareToTwitter(BuildContext context) async {
@@ -260,6 +373,7 @@ class SocialShareWidget extends StatelessWidget {
       print('Twitter share error: $e');
       await _shareToBrowser('twitter', context);
     }
+    Navigator.of(context).pop();
   }
 
   Future<void> _shareToBrowser(String platform, BuildContext context) async {
