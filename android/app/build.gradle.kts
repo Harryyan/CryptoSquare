7c7ad6b1cdc11b42338a8e881440a951f3c56ddc
Harryyan <harryyan1238@gmail.com>
2025-06-13 19:13:55 +1200

    bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         // 配置应用版本信息
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 250612003
+        versionCode = 250614001
         versionName = "2.0.0"
     }
 
